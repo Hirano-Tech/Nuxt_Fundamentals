42a55ef232152d03c827aa10de079dd79e4aa4aa
masakazu-hirano <105690400+masakazu-hirano@users.noreply.github.com>
2022-08-09 09:18:04 +0000

    11: SEO and Meta Tags

--- pages/index.vue
+++ pages/index.vue
@@ -16,6 +16,18 @@
   import Logo from '~/components/Logo.vue'
 
   export default {
+    head() {
+      return {
+        title: 'Home Page 🍕',
+        meta: [
+          { name: 'twitter:title', content: 'Nuxt Fundamentals by Vue School' },
+          { name: 'twitter:description', content: 'Nuxt + Vue School = 🍕' },
+          { name: 'twitter:image', content: 'https://i.imgur.com/UYP2umJ.png' },
+          { name: 'twitter:card', content: 'summary_large_image' },
+        ]
+      }
+    },
+
     components: {
       Logo
     },
--- pages/posts/_id.vue
+++ pages/posts/_id.vue
@@ -1,4 +1,5 @@
 <template>
+<template>
   <div class='container'>
     <article>
       <h1 class='title'>{{ post.title }}</h1>
@@ -18,6 +19,18 @@
 
 <script>
   export default {
+    head() {
+      return {
+        title: this.post.title,
+        meta: [
+          { name: 'twitter:title', content: this.post.title },
+          { name: 'twitter:description', content: this.post.content },
+          { name: 'twitter:image', content: 'https://i.imgur.com/UYP2umJ.png' },
+          { name: 'twitter:content', content: 'summary_large_image' }
+        ]
+      }
+    },
+
     data() {
       return {
         id: this.$route.params.id
